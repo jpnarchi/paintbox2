26c6ae31b2d6026dc1608e5b43be971f1361766b
jpnarchi <142521385+jpnarchi@users.noreply.github.com>
2025-01-23 18:53:51 -0600

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -30,7 +30,7 @@ const Navbar = () => {
           <div className="flex-shrink-0">
             <Link to="/" className="flex items-center">
               <img 
-                src="/logo.png" 
+                src="logo.png" 
                 alt="PaintBOX" 
                 className="h-8 w-auto"
               />

2b3e5f8103cf513ddf21fa1b89784ab529525e9b
jpnarchi <142521385+jpnarchi@users.noreply.github.com>
2025-01-23 18:47:20 -0600

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -29,7 +29,6 @@ const Navbar = () => {
         <div className="flex justify-between items-center h-20">
           <div className="flex-shrink-0">
             <Link to="/" className="flex items-center">
-              {/logo.webp}
               <img 
                 src="/logo.webp" 
                 alt="PaintBOX" 
